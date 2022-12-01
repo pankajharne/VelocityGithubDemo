41c2ef694817a1e6100cbc74af2bd96b950b7b4e
pankajharne <pankajharne84@gmail.com>
2022-12-01 22:05:19 +0530

    Update DashPageTest.java
    Co-Authored-By: sevakselokar <119339304+sevakselokar@users.noreply.github.com>

--- DalalStreetDemo1/src/test/java/com/ds/testLayer/DashPageTest.java
+++ DalalStreetDemo1/src/test/java/com/ds/testLayer/DashPageTest.java
@@ -6,7 +6,7 @@ public class DashPageTest {
 
           System.out.println("Dash Page Change");
           System.out.println("Dash Page Change-----------------2");
-          System.out.println("Dash Page Change-----------1------2");
+         
 
 
 	}

10babffcbdfb8957acd83371e9e5075b2c899f3d
pankajharne <pankajharne84@gmail.com>
2022-12-01 18:18:31 +0530

    new changes for practice

--- DalalStreetDemo1/src/test/java/com/ds/testLayer/DashPageTest.java
+++ DalalStreetDemo1/src/test/java/com/ds/testLayer/DashPageTest.java
@@ -5,6 +5,7 @@ public class DashPageTest {
 	public static void main(String[] args) {
 
           System.out.println("Dash Page Change");
+          System.out.println("Dash Page Change----------------2");
 
 	}
 

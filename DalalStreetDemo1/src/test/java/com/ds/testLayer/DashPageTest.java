d89123b66feb56b5a88d04e06ed9fa5acc42024f
pankajharne <pankajharne84@gmail.com>
2022-12-01 21:54:22 +0530

    new changes ...
    Co-Authored-By: sevakselokar <119339304+sevakselokar@users.noreply.github.com>

--- DalalStreetDemo1/src/test/java/com/ds/testLayer/DashPageTest.java
+++ DalalStreetDemo1/src/test/java/com/ds/testLayer/DashPageTest.java
@@ -5,7 +5,9 @@ public class DashPageTest {
 	public static void main(String[] args) {
 
           System.out.println("Dash Page Change");
-          System.out.println("Dash Page Change----------------2");
+          System.out.println("Dash Page Change-----------------2");
+          System.out.println("Dash Page Change-----------1------2");
+
 
 	}
 
